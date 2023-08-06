21bd53f770fbac6e642659ac885863a9ceea683e
Ahmedarts817 <94564474+Ahmedarts817@users.noreply.github.com>
2023-08-06 04:32:43 -0700

    Update reviewModel.js
    ff

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Product = require("./productModel");
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -27,5 +28,50 @@ const reviewSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+// populate user's name
+reviweSchema.pre(/^find/, function (next) {
+  this.populate({ path: "user", select: "name" });
+  next();
+});
+
+//calculating the average of rating of the product
+reviewSchema.statics.calcAverageRatingsAndQuantity = async function (
+  productId
+) {
+  const result = await this.aggregate([
+    //stage 1 get all reviewsof the same product
+    {
+      $match: { product: productId },
+    },
+    // stage 2 group reviews baed on productId, avgRatingand ,ratingsquantity.
+    {
+      $group: {
+        _id: "product",
+        averageRating: { $avg: $ratings },
+        ratingsQuantity: { $sum: 1 },
+      },
+    },
+  ]);
+  // console.log(result)
+  if (result.length > 0) {
+    await Product.findByIdAndUpdate(productId, {
+      ratingsAverage: result[0].avgRatings,
+      ratingsQuantity: result[0].ratingsQuantity,
+    });
+  } else {
+    await Product.findByIdAndUpdate(productId, {
+      ratingsAverage: 0,
+      ratingsQuantity: 0,
+    });
+  }
+};
+revieSchema.post("save", async function () {
+  await this.constructor.calcAverageRatingsAndQuantity(this.product);
+});
+revieSchema.post("remove", async function () {
+  await this.constructor.calcAverageRatingsAndQuantity(this.product);
+});
+
 const reviewModel = mongoose.model("Review", reviewSchema);
 module.exports = reviewModel;
