17807845c2541c3239eb34d570010b6aa9dcc810
ahmedarts817 <ahmedarts817@gmail.com>
2023-09-03 06:19:38 -0700

    onrender

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const reviewRoute = require("./routes/reviewRoute");
 const couponRoute = require("./routes/couponRoute");
 const cartRoute = require("./routes/cartRoute");
 const orderRoute = require("./routes/orderRoute");
-const orderService = require("./services/orderService");
+const { webhookCheckout } = require("./services/orderService");
 // Connect with db
 dbConnection();
 
@@ -52,9 +52,9 @@ app.use("/api/v1/coupons", couponRoute);
 app.use("/api/v1/cart", cartRoute);
 app.use("/api/v1/orders", orderRoute);
 app.post(
-  "/checkout-webhook",
+  "/webhook-checkout",
   express.raw({ type: "application/json" }),
-  orderService.webhookCheckout
+  webhookCheckout
 );
 
 app.all("*", (req, res, next) => {
