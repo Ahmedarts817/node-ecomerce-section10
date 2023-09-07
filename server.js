61abde714a7abfba66e95ec211feb1bde2dbf03d
ahmedarts817 <ahmedarts817@gmail.com>
2023-09-08 02:26:57 +0300

    g

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const dotenv = require("dotenv");
 const morgan = require("morgan");
 const cors = require("cors");
 const compression = require("compression");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const mongoSanitaize = require("express-mongo-sanitaize");
+const xss = require("xss-clean");
 
 dotenv.config({ path: "config.env" });
 const ApiError = require("./utils/apiError");
@@ -29,11 +33,42 @@ dbConnection();
 const app = express();
 
 // Middlewares
-app.use(express.json());
+app.post(
+  "/checkout-webhook",
+  express.raw({ type: "application/json" }),
+  webhookCheckout
+);
+app.use(express.json({ limit: "20k" }));
 app.use(express.static(path.join(__dirname, "uploads")));
 app.use(cors());
 app.options("*", cors());
 app.use(compression());
+
+// middeleware to limit each ip for 100 requsts per window (per 15 minutes)
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+app.use("/api", limiter);
+// middleware to protect against http request pollution attacks
+
+app.use(
+  hpp({
+    whitelist: [
+      "price",
+      "sold",
+      "quantity",
+      "ratingsAverage",
+      "ratingsQuantity",
+    ],
+  })
+);
+// input data sanitization
+app.use(mongoSanitaize());
+// input cross side script sanitization
+app.use(xss());
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
@@ -51,11 +86,6 @@ app.use("/api/v1/reviews", reviewRoute);
 app.use("/api/v1/coupons", couponRoute);
 app.use("/api/v1/cart", cartRoute);
 app.use("/api/v1/orders", orderRoute);
-app.post(
-  "/checkout-webhook",
-  express.raw({ type: "application/json" }),
-  webhookCheckout
-);
 
 app.all("*", (req, res, next) => {
   next(new ApiError(`Can't find this route: ${req.originalUrl}`, 400));
