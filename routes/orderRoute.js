aaf6603ff3e08ccfb4e9521a130aa2dc192965ed
ahmedarts817 <ahmedarts817@gmail.com>
2023-11-03 01:22:00 +0200

    jjj

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -9,6 +9,7 @@ const {
   updateOrderToPaid,
   updateOrderToDelivered,
   checkoutSession,
+  filterOrderForLoggedUser
 } = require("../services/orderService");
 router.use(authService.protected);
 router.get(
@@ -19,7 +20,7 @@ router.get(
 router.route("/:cartId").post(authService.allowedTo("user"), createCashOrder);
 router
   .route("/")
-  .get(authService.allowedTo("user", "admin", "manager"), findAllOrders);
+  .get(authService.allowedTo("user", "admin", "manager"), filterOrderForLoggedUser,findAllOrders);
 router
   .route("/:id/pay")
   .get(authService.allowedTo("admin", "manager"), findSpecificOrder);
